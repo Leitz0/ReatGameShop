5077881cc0ece7c660a8ec35d51e9d0359f23e1f
juliacoelho3 <juliacoelho3@gmail.com>
2024-06-10 16:12:15 -0300

    Estilizaçao da pagina de login

--- reactGame/src/Pages/Login.jsx
+++ reactGame/src/Pages/Login.jsx
@@ -2,20 +2,25 @@ import { useState, useContext, useEffect } from "react";
 import { useHistory, Link } from "react-router-dom";
 import Context from "../context/Context";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import VpnKey from "@mui/icons-material/VpnKey";
 import api from "../api/api";
 import ModalSenhaIncorreta from "../components/ModalSenhaIncorreta";
 import {
-  Input,
-  InputLabel,
-  InputAdornment,
-  FormControl,
+  TextField,
   Button,
+  Container,
+  Typography,
   Box,
+  Grid,
+  IconButton,
+  InputAdornment,
 } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [senhaIncorreta, setSenhaIncorreta] = useState(false);
 
   const history = useHistory();
@@ -44,75 +49,101 @@ const Login = () => {
     }
   };
 
+  const handleCancel = () => {
+    history.push("/produtos");
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "80vh",
-        backgroundColor: "#CBD6EB",
-        position: "relative",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, 12%)",
-        width: "30%",
-        borderRadius: 3,
-        boxShadow: 4,
-      }}
-    >
+    <Container maxWidth="sm">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Box p={3} boxShadow={3} borderRadius={2}>
+            <Typography variant="h4" gutterBottom style={{ color: "#364B74" }}>
+             Login
+            </Typography>
+            <form onSubmit={handleLogin}>
+              <TextField
+                label="E-mail"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                label="Senha"
+                variant="outlined"
+                type={showPassword ? "text" : "password"}
+                fullWidth
+                margin="normal"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <VpnKey />
+                    </InputAdornment>
+                  ),
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setShowPassword(!showPassword)}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Box display="flex" justifyContent="space-between" mt={2}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  style={{
+                    marginRight: "10px",
+                    backgroundColor: "#364B74",
+                    color: "#FFFFFF",
+                  }}
+                >
+                  Login
+                </Button>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  onClick={handleCancel}
+                  style={{ backgroundColor: "#FFFFFF", color: "#364B74" }}
+                >
+                  Cancelar
+                </Button>
+              </Box>
+              <Box mt={2} textAlign="center">
+                <Typography variant="body2">
+                  Não tem uma conta? <Link to="/cadastro">Registrar</Link>
+                </Typography>
+              </Box>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
       <ModalSenhaIncorreta open={senhaIncorreta} setOpen={setSenhaIncorreta} />
-      <form onSubmit={handleLogin} style={{ textAlign: "center" }}>
-        <h2>Realizar acesso</h2>
-
-        <FormControl variant="standard" style={{ textAlign: "center" }}>
-          <InputLabel htmlFor="input-with-icon-adornment">E-mail</InputLabel>
-          <Input
-            id="input-with-icon-adornment"
-            type="text"
-            placeholder="E-mail"
-            required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            startAdornment={
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <br></br>
-
-        <FormControl variant="standard" style={{ textAlign: "center" }}>
-          <InputLabel htmlFor="input-with-icon-adornment">Senha</InputLabel>
-          <Input
-            id="input-with-icon-adornment"
-            type="password"
-            placeholder="Senha"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            startAdornment={
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <br></br>
-
-        <Button type="submit" variant="contained" color="success">
-          Login
-        </Button>
-
-        <div className="signup-link" style={{ textAlign: "center" }}>
-          <p>
-            Não tem uma conta? <br></br>
-            <Link to="/cadastro">Registar</Link>
-          </p>
-        </div>
-      </form>
-    </Box>
+    </Container>
   );
 };
 
