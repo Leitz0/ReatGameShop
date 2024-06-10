807de65097c5dff5b3394dfd9ee59bb5dd1f804d
Leitz0 <162743011+Leitz0@users.noreply.github.com>
2024-06-10 18:53:58 -0300

    Pedido visual ajustado

--- reactGame/src/components/CardPedido.jsx
+++ reactGame/src/components/CardPedido.jsx
@@ -1,22 +1,65 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 const CardPedido = ({ pedido }) => {
-  const { id, idUSer, valorTotal, itens } = pedido
+  const { id, idUSer, valorTotal, itens, dataCompra } = pedido
 
   return (
-    <div>
-      <p>{id}</p>
-      <p>{idUSer}</p>
-      <p>{valorTotal}</p>
-    {itens.map(({idProduto,
-      quantidade}) => (
-        <div>
-          <p>{idProduto}</p>
-          <p>{quantidade}</p>
-        </div>
-      ))}
+    <Card sx={{width:'100%', mb: 2}} style={{backgroundColor:'#d3d3ed'}}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" style={{ backgroundColor: '#1976d2', color: 'white' }}>
+          Numero do Pedido: {id}
+        </Typography>
+        <Typography variant="body2" display="block">
+          Total de pedido: {valorTotal.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+        </Typography>
+        <Typography variant="body2">
+          Data da compra: {new Date(dataCompra).toLocaleDateString()}
+        </Typography>
+        {itens.map(({ idProduto, nome,
+          imgUrl, descricao, preco, categoria, quantidade }) => (
+          <CardContent sx={{display:'flex', justifyContent: 'flex-start'}}>
+            <CardMedia
+              component="img"
+              sx={{ width: 140, height: 140, objectFit: "contain" }}
+              image={imgUrl}
+            />
+            <div>
+            <Typography variant="body2" color="text.secondary">
+              Cód do Produto: {idProduto}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {nome}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Categoria: {categoria}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Descrição:
+              <br />
 
+              {descricao}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Quant.: {quantidade}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              Valor: {preco.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+            </Typography>
+            </div>
+          </CardContent>
+        ))}
 
-    </div>
-
+      </CardContent>
+    </Card>
   )
 }
 export default CardPedido
